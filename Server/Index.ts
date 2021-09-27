c7fa016aa20b8b29593e88a88e78ef40c8afa11a
Dermot <contact@dermotobrien.me>
2021-09-27 15:52:43 +0100

    Server route

--- Server/Index.ts
+++ Server/Index.ts
@@ -2,6 +2,11 @@ import express, { Application, NextFunction, Request, Response, Router } from "e
 const app: Application = express();
 const port: string = '8080';
 
+
+app.get('/', (req, res) => { 
+    res.send({ message: 'Server is active', timestamp: new Date() }); 
+});
+
 try {
     app.listen(port, (): void => {
         console.log(`Server is active at http://localhost:${port}`);
