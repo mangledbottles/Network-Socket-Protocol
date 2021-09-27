d013b7e8167c33fedfadafb8e4adbef6e9ad8842
Dermot <contact@dermotobrien.me>
2021-09-27 16:04:42 +0100

    Setup Socket API

--- Server/Index.ts
+++ Server/Index.ts
@@ -2,13 +2,21 @@ import express, { Application, NextFunction, Request, Response, Router } from "e
 const app: Application = express();
 const port: string = '8080';
 
+const server = require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => { 
     res.send({ message: 'Server is active', timestamp: new Date() }); 
 });
 
+io.on('connection', (socket: any) => {
+    console.log(socket)
+    console.log('a user connected');
+});
+
 try {
-    app.listen(port, (): void => {
+    server.listen(port, (): void => {
         console.log(`Server is active at http://localhost:${port}`);
     });
 } catch (error: any) {
