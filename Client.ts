6eeb6e60c4543439de352c322aaf8110a34fd25b
Dermot <contact@dermotobrien.me>
2021-10-08 18:07:59 +0100

    Standardising Port aming Convention

--- Client.ts
+++ Client.ts
@@ -1,6 +1,6 @@
 import dgram from 'dgram';
 import { Buffer } from 'buffer';
-const port: number = 8080;
+const socketPort: number = 8080;
 
 const message = Buffer.from('UDP IOT CONNETION DATA');
 const client = dgram.createSocket('udp4');
@@ -10,7 +10,7 @@ client.on('message', (msg, info) => {
   console.log(`Received ${msg.length} bytes from ${info.address}:${info.port}\n`);
 });
 
-client.send(message, port, 'localhost', (err) => {
+client.send(message, socketPort, 'localhost', (err) => {
   if (err) {
     console.log('Error sending data to Server')
     client.close();
