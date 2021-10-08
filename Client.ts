bbe9360c60c3ec3853cc896d1589e24c46979881
Dermot <contact@dermotobrien.me>
2021-10-08 18:09:45 +0100

    Add documentation to Client

--- Client.ts
+++ Client.ts
@@ -5,11 +5,13 @@ const socketPort: number = 8080;
 const message = Buffer.from('UDP IOT CONNETION DATA');
 const client = dgram.createSocket('udp4');
 
+/** Listen and handle all incoming messages */
 client.on('message', (msg, info) => {
   console.log(`Data received from server: ${msg.toString()}`);
   console.log(`Received ${msg.length} bytes from ${info.address}:${info.port}\n`);
 });
 
+/** Send message to Server */
 client.send(message, socketPort, 'localhost', (err) => {
   if (err) {
     console.log('Error sending data to Server')
