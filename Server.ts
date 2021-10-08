8ad166a8257fa094388cda6f6dbfc99fe320980c
Dermot <contact@dermotobrien.me>
2021-10-08 17:46:54 +0100

    Update Server.ts

--- Server.ts
+++ Server.ts
@@ -1,21 +1,22 @@
 import dgram from "dgram";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
 const port: number = 8080;
 
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
+/** HTTP Server for checking status */
 app.get('/', (req: Request, res: Response) => {``
     res.send({ message: 'Server is active', timestamp: new Date() }); 
 });
 
 const Server = dgram.createSocket('udp4');
 
-/** Broker listen to all subscribers and manage protocol */
-
 Server.on('error', (err) => {
   console.log(`Server error:\n${err.stack}`);
   Server.close();
 });
 
+/** Receive Messages */
 Server.on('message', (msg, rinfo) => {
   console.log(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 
@@ -30,12 +31,10 @@ Server.on('message', (msg, rinfo) => {
 
 /** Launch server and listen on given port */
 try {
-
   Server.on('listening', () => {
     const address = Server.address();
     console.log(`Server listening ${address.address}:${address.port}`);
   });
-
 
   Server.bind(port, (): void => {
     console.log(`UDP Datagram Server is active at http://localhost:${port}`);
